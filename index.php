6ee45e9e9f08adb2a5dc8fefc3deccc86ff73842
philostein <phildooher@gmail.com>
2011-02-10 03:24:41 -0800

    Test html

--- index.php
+++ index.php
@@ -10,24 +10,141 @@
 <link href="plugins/styles.css" rel="stylesheet" type="text/css"  />
 </head>
 <body>
-	<center>
-	<img src="images/quicksilver.png" width="64px" height="64px" alt="QS logo" />
-	<br/>
-	<h3><img src="images/quicksilver-title.png" width="160px" height="31px" alt="Quicksilver" /></h3>
-	<p style="color:#151515;font-size:20pt;margin-top:-10px;	font-variant: small-caps;
-">Collated, Finally</p>
-	<div class="opener" style="padding-top:40px">
-	<p>Hopefully this site will be the 'new' home for Quicksilver and everything that comes with it.<br />
-The idea is to bring everything that's been happening to Quicksilver since it went open source together in one beautiful, unified place.</p>
-<p>		At the moment we have the <a href="plugins">plugins repository</a>, as well as a <a href="wiki">wiki</a> that's being set up.</p>
-<p>If you've always wanted to help with Quicksilver, but have never had the Cocoa coding skills, then this may just be your chance!<br />
-You know PHP, HTML, MySQL, or anything webby? You want to help right now?! Cool, just email help@qsapp.com :)</p>
-</div>
+	<body bgcolor="#515151" link="#FFFFFF" vlink="#CCCCCC" >
 
-			
-	<p style="position:absolute;bottom:10px;color:#d1d1d1;font-size:8pt;">Set up by &copy; Patrick Robertson, <?php echo date('Y'); ?><br />
-		Some styles and images taken from the <a style="color:#d1d1d1" href="http://blacktree.com">http://blacktree.com</a>. I hope you don't mind Alcor &lt;3
-		</p>
-	</center>	
+    <div id="wrapper">
+        <table>
+        
+        <table border=0 width=60%   cellpadding="0" background="http://static.tumblr.com/rf8jmdi/PU2lg56xd/lovequicksilver_table_background_515151_gray_translucent.png">
+            <tr>
+            <td>
+            <table>
+                <tr>
+
+                    <td valign="top" align="left" width=40 border=0>
+                    </td>
+       
+                    <td valign="top" align="left">
+                        <table>
+                        <tr>
+                        <td>
+                        <img src="http://static.tumblr.com/rf8jmdi/kpVlg6c60/quicksilver_128.png" />
+                        </td>
+                
+                        <td>
+                            <div id="title">
+                                <a href="http://QSApp.com">Quicksilver</a>
+                            </div>
+                                
+                            <div id="subtitle" align="middle">
+                        A second home at <a href="http://QSApp.com">QSApp.com</a>
+                            </div>
+                        </td>
+                        <tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            </td>
+            </tr>
+            
+            <tr>
+            <td>
+            <table>
+            <tr>
+                <td valign="top" align="left" width=65 border=0>
+                </td>
+                
+                <td valign="top" align="left" cellpadding=0 >
+                    <div id="blog">
+                    
+                        <script type="text/javascript" src="http://lovequicksilver.tumblr.com/js"></script> 
+                    </div>
+                </td>
+            </tr>
+            </table>
+            <td>
+            </tr>
+        </table>
+                                  
+        
+        <div id="sidebar">
+        <div id="body3">
+            <table>
+            
+                    <table  align="middle">
+                        <div id="story" align="middle">
+                        <p>This site is a new home for Quicksilver, <br />and everything that comes with it.<br />
+We're bringing everything that's been happening<br /> to Quicksilver since it went open source<br /> to one beautiful, unified place.</p>
+<p>		There's the <a href="http://qsapp.com/plugins">plugin repository</a>,<br /> as well as a <a href="http://qsapp.com/wiki">wiki</a> of useful information.</p>
+You know PHP, HTML, MySQL, or anything webby? <br />Or you just want to help with Quicksilver right now? <br />Then go to <a href="http://qsapp.com/wiki/QSApp.com">this</a> page.</p>
+                        </div>
+                    </table>
+                                <table width=290>
+                                    <!--
+                                    <td align="right" width=105>
+                                        Download:<br />
+                                        Download:<br />
+                                        Plugins:<br />
+                                        Wiki:<br />
+                                        Discussion:<br />
+                                        Email:<br />
+                                        <br />
+                                    </td>
+                                    -->
+                                    
+                                    <td align="middle">
+                                        <a href="http://www.blacktree.com/">Download Quicksilver</a><br />
+                                        <a href="http://qsapp.com/plugins">Plugin repository</a><br />
+                                        <a href="http://qsapp.com/wiki">Wiki</a><br />
+                                        <a href="http://bit.ly/ggXX9W">Discussion</a><br />
+                                        help@qsapp.com<br />
+                                        <br />
+                                    </td>
+                                </table> 
+
+                            <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+                            <script>
+                        new TWTR.Widget({
+                        version: 2,
+            type: 'profile',
+            rpp: 12,
+            interval: 300,
+            width: 290,
+            height:200,
+            theme: {
+                shell: {
+                    background: '#311768', <!--321557-->
+                    color: '#b7b7b7'
+                },
+                tweets: {
+                    background: '#515151',
+                    color: '#b7b7b7',
+                    links: '#e8e8e8'
+                }
+            },
+            features: {
+                scrollbar: true,
+                loop: false,
+                live: true,
+                hashtags: true,
+                timestamp: true,
+                avatars: true,
+                behavior: 'all'
+            }
+        }).render().setUser('LoveQuicksilver').start();
+                            </script>
+                    
+                    <div  align="middle">
+                    <p style=" color:#d1d1d1; font-size:8pt;">Set up by &copy; <a href="http://patjack.co.uk">Patrick Robertson</a>, 2011<br />
+		Some styles taken from the <a style="color:#fff" href="http://blacktree.com">Blacktree.com</a><br />
+        Background photo &copy; <a href="http://www.flickr.com/photos/sovietuk/3150723485/in/faves-57485932@N05/">Rick Harrison</a>
+                    </p>
+                    </div>
+            </table>
+        </div>
+        </div>
+     </table>   
+    </div>
 </body>
 </html>
